907a682dcacf118a09ba03f9aa0578bf87904da1
Red Wang <redxoxored@gmail.com>
2023-10-31 13:06:53 +0800

    Initial Commit:

---
+++ DI Part 1 - Delegate/DI Part 1 - Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI Part 1 - Delegate
+//
+//  Created by Red Wang on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
